dac727e8c1dd3530f811807f4618343b28563793
unknown <Han Martin@NET1.local>
2026-04-14 07:51:49 +0200

    Kész get.js és változás a htmlben

--- js/kosar.js
+++ js/kosar.js
@@ -1,56 +1,28 @@
-export let kosar = [];
+let kosar = [];
 
-export function megjelenites(termekek) {
-    const tarolo = document.getElementById("termekek");
-    tarolo.innerHTML = "";
-
-    termekek.forEach(t => {
-        tarolo.innerHTML += `
-        <div class="kartya">
-            <img src="${t.thumbnail}">
-            <h3>${t.title}</h3>
-            <p>${t.description}</p>
-            <p>${t.price} Ft</p>
-            <button onclick="kosarba(${t.id}, '${t.title}', ${t.price})">
-                Kosárba
-            </button>
-        </div>
-        `;
-    });
-}
-
-export function kosarba(id, nev, ar) {
-    const van = kosar.find(t => t.id === id);
+export function kosarba(termek) {
+    const van = kosar.find(t => t.id === termek.id);
 
     if (van) {
         van.db++;
     } else {
-        kosar.push({ id, nev, ar, db: 1 });
+        kosar.push({ ...termek, db: 1 });
     }
 
-    kosarFrissites();
+    osszegFrissites();
 }
 
-function kosarFrissites() {
-    const lista = document.getElementById("kosarLista");
-    lista.innerHTML = "";
-
-    kosar.forEach(t => {
-        lista.innerHTML += `
-            <li>${t.nev} - ${t.db} db - ${t.ar * t.db} Ft</li>
-        `;
-    });
-
-    document.getElementById("vegosszeg").textContent =
-        kosarOsszeg(kosar);
+export function kosarOsszeg(lista) {
+    return lista.reduce((osszeg, t) => {
+        return osszeg + t.price * t.db;
+    }, 0);
 }
 
-export function kosarOsszeg(kosar) {
-    let osszeg = 0;
+function osszegFrissites() {
+    const veg = kosarOsszeg(kosar);
+    document.getElementById("vege-osszeg").innerText = veg + "ft";
+}
 
-    kosar.forEach(t => {
-        osszeg += t.ar * t.db;
-    });
-
-    return osszeg;
+export function getKosar() {
+    return kosar;
 }
