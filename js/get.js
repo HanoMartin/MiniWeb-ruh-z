f931448d477238b9764d649da2bf0dbc83e6ea3e
unknown <Han Martin@NET1.local>
2026-04-14 07:32:00 +0200

    Getjs fejlesztése kész

--- js/get.js
+++ js/get.js
@@ -1,8 +1,46 @@
-import { megjelenites } from "./kosar.js";
+import { kosarba } from "./kosar.js";
 
-export async function termekekBetoltese() {
-    const valasz = await fetch("https://dummyjson.com/products");
-    const adat = await valasz.json();
+const container = document.querySelector(".row");
 
-    megjelenites(adat.products);
+async function termekekBetoltese() {
+    const res = await fetch("https://dummyjson.com/products");
+    const data = await res.json();
+
+    megjelenites(data.products);
 }
+
+function megjelenites(termekek) {
+    termekek.forEach(t => {
+        const div = document.createElement("div");
+        div.className = "col-ml-3 float-start sm-12";
+
+        div.innerHTML = `
+        <div class="kartyak">
+            <img src="${t.thumbnail}" class="card-img-top">
+            <div class="card-body d-flex flex-column">
+                <h5 class="kartya-nev">${t.title}</h5>
+                <p class="kartya-leiras">${t.description}</p>
+                <div class="mt-auto">
+                    <h4><span class="ar">${t.price}ft</span></h4>
+                    <h4>Darabszám: <span class="db">1</span></h4>
+                    <button class="btn btn-primary float-end">Kosárba</button>
+                </div>
+            </div>
+        </div>
+        `;
+
+        const gomb = div.querySelector("button");
+
+        gomb.addEventListener("click", () => {
+            kosarba({
+                id: t.id,
+                title: t.title,
+                price: t.price
+            });
+        });
+
+        container.appendChild(div);
+    });
+}
+
+termekekBetoltese();
