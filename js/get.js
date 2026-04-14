49934c414334f85fde5a203bf80bbcd0d7bc1efe
unknown <Han Martin@NET1.LOCAL>
2026-04-14 08:55:35 +0200

    Változott és végleges get.js

--- js/get.js
+++ js/get.js
@@ -1,51 +1,66 @@
 import { kosarba } from "./kosar.js";
+import { sajatTermekek } from "./post.js";
 
-const container = document.querySelector(".container .row");
+export let lastProducts = [];
 
-async function termekekBetoltese() {
-    try {
-        const res = await fetch("https://dummyjson.com/products");
-        const data = await res.json();
 
-        megjelenites(data.products);
-    } catch (error) {
-        console.error("Hiba történt:", error);
-    }
+const container = document.getElementById("termekek");
+
+async function loadProducts() {
+    const res = await fetch("https://dummyjson.com/products");
+    const data = await res.json();
+
+    render(data.products);
 }
 
-function megjelenites(termekek) {
-    termekek.forEach(t => {
-        const div = document.createElement("div");
+function render(products) {
+    container.innerHTML = "";
 
-        div.className = "col-ml-3 float-start sm-12";
+    products.forEach(p => {
+        const div = document.createElement("div");
+        div.className = "col-12 col-sm-6 col-md-4 col-lg-3";
 
         div.innerHTML = `
-        <div class="kartyak">
-            <img src="${t.thumbnail}" class="card-img-top" alt="${t.title}">
+        <div class="card shadow-sm h-100">
+            <img src="${p.thumbnail}" class="card-img-top">
+
             <div class="card-body d-flex flex-column">
-                <h5 class="kartya-nev">${t.title}</h5>
-                <p class="kartya-leiras">${t.description}</p>
-                <div class="mt-auto">
-                    <h4><span class="ar">${t.price}ft</span></h4>
-                    <h4>Darabszám: <span class="db">1</span></h4>
-                    <button class="btn btn-primary float-end">Kosárba</button>
+
+                <h5>${p.title}</h5>
+
+                <p class="small flex-grow-1">
+                    ${p.description.substring(0, 60)}...
+                </p>
+
+                <span class="badge bg-primary mb-2">
+                    Készlet: ${p.stock}
+                </span>
+
+                <div class="d-flex justify-content-between align-items-center">
+
+                    <b>${p.price} $</b>
+
+                    <button class="btn btn-primary btn-sm">
+                        Kosár
+                    </button>
+
                 </div>
             </div>
         </div>
         `;
 
-        const gomb = div.querySelector("button");
-
-        gomb.addEventListener("click", () => {
-            kosarba({
-                id: t.id,
-                title: t.title,
-                price: t.price
-            });
+        div.querySelector("button").addEventListener("click", () => {
+            if (p.stock > 0) {
+                kosarba(p.id, p.title, p.price, p.stock);
+                p.stock--;
+                render(products);
+            } else {
+                alert("Elfogyott a készlet!");
+            }
         });
 
         container.appendChild(div);
     });
 }
 
-termekekBetoltese();
+loadProducts();
