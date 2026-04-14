9d4d38d46103bba415001d9481e3f32dd8f844ad
unknown <Han Martin@NET1.local>
2026-04-14 07:48:55 +0200

    Kész get.js + html változtatás

--- js/get.js
+++ js/get.js
@@ -1,22 +1,27 @@
 import { kosarba } from "./kosar.js";
 
-const container = document.querySelector(".row");
+const container = document.querySelector(".container .row");
 
 async function termekekBetoltese() {
-    const res = await fetch("https://dummyjson.com/products");
-    const data = await res.json();
+    try {
+        const res = await fetch("https://dummyjson.com/products");
+        const data = await res.json();
 
-    megjelenites(data.products);
+        megjelenites(data.products);
+    } catch (error) {
+        console.error("Hiba történt:", error);
+    }
 }
 
 function megjelenites(termekek) {
     termekek.forEach(t => {
         const div = document.createElement("div");
+
         div.className = "col-ml-3 float-start sm-12";
 
         div.innerHTML = `
         <div class="kartyak">
-            <img src="${t.thumbnail}" class="card-img-top">
+            <img src="${t.thumbnail}" class="card-img-top" alt="${t.title}">
             <div class="card-body d-flex flex-column">
                 <h5 class="kartya-nev">${t.title}</h5>
                 <p class="kartya-leiras">${t.description}</p>
